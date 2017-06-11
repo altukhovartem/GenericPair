198164c08f35a45fec545031708304bbca8d4ffe
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-06-11 13:09:46 +0300

    First commit

---
+++ GenericPair/GenericPair/Pair.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPair
+{
+    public class Pair<T,K>
+    {
+        private T _first;
+        private K _second;
+
+        public T First
+        {
+            get { return _first; }        
+        }
+
+        public K Second
+        {
+            get { return _second; }
+        }
+
+        private Pair(T t, K k)
+        {
+            this._first = t;
+            this._second = k;
+        }
+
+        public static Pair<T, K> Of(T t, K k)
+        {
+            return new Pair<T, K>(t,k);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_first.ToString() + _second.ToString()).GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Pair<T,K > currentPair = null;
+            if (obj is Pair<T,K>)
+            {
+                currentPair = obj as Pair<T,K>;
+                if (this.GetHashCode() == currentPair.GetHashCode())
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+
+        }
+    }
+}
---
+++ GenericPair/GenericPair/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<int, String> pair = Pair<int, string>.Of(1, "hello");
+            int i = pair.First; // 1
+            String s = pair.Second; // "hello"
+
+            Pair<int, String> pair2 = Pair<int, string>.Of(1, "hello");
+            bool mustBeTrue = pair.Equals(pair2); // true!
+            bool mustAlsoBeTrue = pair.GetHashCode() == pair2.GetHashCode(); // true!
+
+        }
+    }
+}
